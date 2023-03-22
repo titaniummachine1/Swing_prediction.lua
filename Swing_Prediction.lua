41e0b448c72873282fc7a6eacd45bfd3eab8513a
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2023-03-22 18:54:13 +0100

    consistent and redability

--- Swing_Prediction.lua
+++ Swing_Prediction.lua
@@ -23,22 +23,22 @@ local flTimer = 0          -- Timer for Fake Latency
 local c2Timer = 0          -- Timer for Battle CryoWeaponmAutoweapon raytracing
 local c2Timer2 = 0         -- Timer for ^ to prevent spamming
 local mfTimer = 0          -- Timer for Medic Finder
-
+tickRate = 66 -- game tick rate
 --[[ Menu ]]--
 local menu = MenuLib.Create("Swing Prediction", MenuFlags.AutoSize)
 menu.Style.TitleBg = { 205, 95, 50, 255 } -- Title Background Color (Flame Pea)
 menu.Style.Outline = true                 -- Outline around the menu
 
---[[menu:AddComponent(MenuLib.Button("Debug", function() -- Disable Weapon Sway (Executes commands)
+menu:AddComponent(MenuLib.Button("Debug", function() -- Disable Weapon Sway (Executes commands)
     client.SetConVar("cl_vWeapon_sway_interp",              0)             -- Set cl_vWeapon_sway_interp to 0
     client.SetConVar("cl_jiggle_bone_framerate_cutoff", 0)             -- Set cl_jiggle_bone_framerate_cutoff to 0
     client.SetConVar("cl_bobcycle",                     10000)         -- Set cl_bobcycle to 10000
     client.SetConVar("sv_cheats", 1)                                    -- debug fast setup
     client.SetConVar("mp_disable_respawn_times", 1)
     client.SetConVar("mp_respawnwavetime", -1)
-end, ItemFlags.FullWidth))]]
+end, ItemFlags.FullWidth))
 
-local debug = menu:AddComponent(MenuLib.Checkbox("indicator", false))
+local debug = menu:AddComponent(MenuLib.Checkbox("indicator", true))
 local Swingpred = menu:AddComponent(MenuLib.Checkbox("Enable", true))
 local mtimeahead   = menu:AddComponent(MenuLib.Slider("distance ahead",    0, 300, 250))
 
@@ -46,48 +46,44 @@ local mtimeahead   = menu:AddComponent(MenuLib.Slider("distance ahead",    0, 30
 -- local mRageSpecKill = menu:AddComponent(MenuLib.Checkbox("Rage Spectator Killbind", false)) -- fuck you "pizza pasta", stop spectating me
 --local mRemovals     = menu:AddComponent(MenuLib.MultiCombo("Removals", Removals, ItemFlags.FullWidth)) -- Remove RTD and HUD Texts
 
+function GameData()
+    local data = {}
+
+    -- Get local player data
+    data.pLocal = entities.GetLocalPlayer()     -- Immediately set "pLocal" to the local player (entities.GetLocalPlayer)
+    data.pWeapon = data.pLocal:GetPropEntity("m_hActiveWeapon")
+    data.pWeaponDefIndex = data.pWeapon:GetPropInt("m_iItemDefinitionIndex")
+    data.pWeaponDef = itemschema.GetItemDefinitionByID(data.pWeaponDefIndex)
+    data.pWeaponName = data.pWeaponDef:GetName()
+    data.pUsingProjectileWeapon = (data.pWeaponName == "CTFRocketLauncher" or data.pWeaponName == "CTFCannon")
+    -- Get player data for all players in the game
+
+    -- Check if local player is invisible
+    data.sneakyboy = (data.pLocal:InCond(4) or data.pLocal:InCond(2) or data.pLocal:InCond(13) or data.pLocal:InCond(9))
+
+    return data
+end
+
+
+local function GetClosingSpeed(mDistance, mPastdistance)
+    local speedPerTick = mDistance - (mPastdistance or 0)
+    local closingSpeed = -(speedPerTick * tickRate / 1000) -- closing speed in units/ms
+    return (closingSpeed or 0) -- difference in distance between current and previous tick
+end
+
+
+
 --[[ Code needed to run 66 times a second ]]--
-local function OnCreateMove(pCmd)                    -- Everything within this function will run 66 times a second
-    local pLocal = entities.GetLocalPlayer()         -- Immediately set "pLocal" to the local player (entities.GetLocalPlayer)
+local function OnCreateMove(pCmd, gameData)
+    gameData = GameData()  -- Update gameData with latest information
+
+    local pLocal, pWeapon, pWeaponDefIndex = gameData.pLocal, gameData.pWeapon, gameData.pWeaponDefIndex
+    -- Use pLocal, pWeapon, pWeaponDefIndex, etc. as needed
+
     if not pLocal then return end                    -- Immediately check if the local player exists. If it doesn't, return.
 
-    --[[ Features that require access to the weapon ]]--
-    if pLocal:IsAlive() == false then return end
-        local pWeapon         = pLocal:GetPropEntity( "m_hActiveWeapon" )            -- Set "pWeapon" to the local player's active weapon
-        local pWeaponDefIndex = pWeapon:GetPropInt( "m_iItemDefinitionIndex" )       -- Set "pWeaponDefIndex" to the "pWeapon"'s item definition index
-        local pWeaponDef      = itemschema.GetItemDefinitionByID( pWeaponDefIndex )  -- Set "pWeaponDef" to the local "pWeapon"'s item definition
-        local pWeaponName     = pWeaponDef:GetName()                                 -- Set "pWeaponName" to the local "pWeapon"'s actual name
-        if not pWeapon then return end                                               -- If "pWeapon" is not set, break
-    if (pWeapon == "CTFRocketLauncher") or (pWeapon == "CTFCannon") then        -- If the local player's active weapon is a projectile weapon (this doesn't work for some reason????)
-        pUsingProjectileWeapon  = true                                               -- Set "pUsingProjectileWeapon" to true
-    else pUsingProjectileWeapon = false end                                          -- Set "pUsingProjectileWeapon" to false
 
-
-
-    --[[ Features that need to iterate through all players ]]
-    local players = entities.FindByClass("CTFPlayer")                              -- Create a table of all players in the game
-    for k, vPlayer in pairs(players) do                                            -- For each player in the game
-        if vPlayer:IsValid() == false then goto continue end                       -- Check if each player is valid
-    local vWeapon = vPlayer:GetPropEntity("m_hActiveWeapon")                       -- Set "vWeapon" to the player's active weapon
-    if vWeapon ~= nil then                                                         -- If "vWeapon" is not nil
-        local vWeaponDefIndex = vWeapon:GetPropInt("m_iItemDefinitionIndex")       -- Set "vWeaponDefIndex" to the "vWeapon"'s item definition index
-        --local vWeaponDef      = itemschema.GetItemDefinitionByID(vWeaponDefIndex)  -- Set "vWeaponDef" to the local "vWeapon"'s item definition (doesn't work for some reason)
-        --local vWeaponName     = vWeaponDef:GetName()                               -- Set "vWeaponName" to the local "vWeapon"'s actual name (doesn't work for some reason)
-    end
-
-
-    local sneakyboy = false                       -- Create a new variable for if we're invisible or not, set it to false
-    if pLocal:InCond(4) or pLocal:InCond(2) 
-                        or pLocal:InCond(13) 
-                        or pLocal:InCond(9) then  -- If we are in a condition that makes us invisible
-        sneakyboy = true                          -- Set "sneakyboy" to true
-    end
-
-
-        if vPlayer:IsAlive() == false then goto continue end
-        if vPlayer:GetIndex() == pLocal:GetIndex() then goto continue end            -- Code below this line doesn't work if you're the only player in the game.
-        if pLocal:IsAlive() == false then goto continue end                          -- If we are not alive, skip the rest of this code
-
+       
         --if vPlayer:GetTeamNumber() == pLocal:GetTeamNumber() then goto continue end  -- If we are on the same team as the player we are iterating through, skip the rest of this code
 
 
@@ -96,7 +92,7 @@ local function OnCreateMove(pCmd)                    -- Everything within this f
     local LocalPlayer = entities.GetLocalPlayer()
    -- local added_per_shot, bucket_current, crit_fired
     
-    local vPlayerhitbox = 32
+    local vPlayerhitbox = 0
 
     local swingrange = pWeapon:GetSwingRange()
     if swingrange ~= nil then
@@ -111,33 +107,41 @@ if sneakyboy then return end
 
 -- Initialize closest distance and closest player
 if Swingpred:GetValue() then
-    local PlayerClass = LocalPlayer:GetPropInt("m_iClass")
+        local PlayerClass = LocalPlayer:GetPropInt("m_iClass")
+        local closestPlayer = nil
+        closestDistance = 1000
+        Vhitbox_Height = 85
+        local maxDistance = 700
 
-    if PlayerClass == 8 then
-        return
-    end
+    --get pLocal eye level and set vector at our eye level to ensure we cehck distance from eyes
+        local viewOffset = pLocal:GetPropVector("localdata", "m_vecViewOffset[0]")
+        local adjustedHeight = pLocal:GetAbsOrigin() + viewOffset
+        viewheight = (adjustedHeight - pLocal:GetAbsOrigin()):Length()
 
-    local closestPlayer = nil
-    closestDistance = 1000
-    Vhitbox_Height = 85
+        local hitbox_height = Vector3( 0, 0, Vhitbox_Height )
+        local Vheight = Vector3( 0, 0, viewheight )
+
+
+        if PlayerClass == 8 then
+            return
+        end
     
-    
-    local viewOffset = pLocal:GetPropVector("localdata", "m_vecViewOffset[0]")
-    local adjustedHeight = pLocal:GetAbsOrigin() + viewOffset
-    viewheight = (adjustedHeight - pLocal:GetAbsOrigin()):Length()
-
-    local hitbox_height = Vector3( 0, 0, Vhitbox_Height )
-    local Vheight = Vector3( 0, 0, viewheight )
-    -- Find the closest player
-    local maxDistance = 700
     for _, vPlayer in ipairs(players) do
+        local enemy = vPlayer:GetTeamNumber() ~= pLocal:GetTeamNumber()
         -- Only check distance for alive enemies on the other team within maxDistance
-        if vPlayer:IsAlive() and vPlayer:GetTeamNumber() ~= pLocal:GetTeamNumber() then
-            
+        if enemy then
 
-
-
-
+            if vPlayer:GetIndex() == pLocal:GetIndex() then goto continue end            -- Code below this line doesn't work if you're the only player in the game.
+            if pLocal:IsAlive() == false then goto continue end                          -- If we are not alive, skip the rest of this code
+    
+            if vPlayer:IsValid() then
+                local vWeapon = vPlayer:GetPropEntity("m_hActiveWeapon")
+                if vWeapon ~= nil then
+                    local vWeaponDefIndex = vWeapon:GetPropInt("m_iItemDefinitionIndex")
+                    --local vWeaponDef = itemschema.GetItemDefinitionByID(vWeaponDefIndex)
+                    --local vWeaponName = vWeaponDef:GetName()
+                end
+            end
 
             pLocalOrigin = pLocal:GetAbsOrigin() + Vheight
             vPlayerOrigin = vPlayer:GetAbsOrigin()
@@ -155,61 +159,68 @@ if Swingpred:GetValue() then
             distancetop = distVector:Length() - swingrange
 
             -- Update closest player and closest distance
-            if distance < closestDistance and distance <= maxDistance then
+            if distance < closestDistance and distance <= maxDistance and  vPlayer:IsAlive() == true then
                 closestPlayer = vPlayer
                 closestDistance = distance
             end
         end
     end
     
-
-    local stop = false
-    if (pLocal:InCond(17)) and PlayerClass == 4 or PlayerClass == 8 then -- If we are charging (17 is TF_COND_SHIELD_CHARGE)
-        stop = true
-        dynamicstop = 45
-        if (pCmd.forwardmove == 0) then dynamicstop = 30 end -- case if you dont hold w when charging
-        if closestDistance <= dynamicstop and PlayerClass == 4 then
-            pCmd:SetButtons(pCmd:GetButtons() | IN_ATTACK)
-        end
-    end
     -- Check if there is a valid closest player
     if closestPlayer ~= nil then
         -- Calculate estimated hit time based on the closest player's distance
         local distance = closestDistance
         --local hostile = (closestPlayer:GetTeamNumber() == pLocal:GetTeamNumber())
-        
             -- Check if there are enemies in range and predicted hit time is valid
-            
-            -- Swing Prediction
-            local tickRate = 66 -- game tick rate
-            local speedPerTick = distance - (previousDistance or 0) -- difference in distance between current and previous tick
-            local speedperTickfeet = distancefeet - (previousDistancefeet or 0)
-            
-            local closingSpeed = -(speedPerTick * tickRate / 1000) -- closing speed in units/ms
-            local closingSpeedfeet = -(speedperTickfeet * tickRate / 1000)
-            --print(closingSpeed)
-            local isMelee = pWeapon:IsMeleeWeapon()
+
+            local isMelee = pWeapon:IsMeleeWeapon() -- check if using melee weapon
             -- turn input milisecodn value to code
-            local timeAhead = mtimeahead:GetValue()
+            local timeAhead = mtimeahead:GetValue() -- check timeahead set in menu
+
+            --[[-----------------------------Swing Prediction--------------------------------]]
+
+            --calculating eyelevel closing speed
+                local mDistance = distance
+                local mPastdistance = previousDistance
+                closingSpeed = GetClosingSpeed(mDistance, mPastdistance)
+
+            -- calculating feetlevel closing speed towards our eyes
+                local mDistance = distancefeet
+                local mPastdistance = previousDistancefeet
+                closingSpeedfeet = GetClosingSpeed(mDistance, mPastdistance)
+            
+                --print(closingSpeed)
             
             -- Check if enemy is within swing range or melee range
             local withinMeleeRange = distance <= 500
             
-            -- Check if relative speed is greater than 2000 units/ms
+            --[[ Check if relative speed is greater than 2000 units/ms
             if math.abs(closingSpeed) > 3000 then
                 closingSpeed = 0
-            end
+            end]]
             
             -- Calculate estimated hit time in milliseconds
             estHitTime = 0
-            if closingSpeed ~= 0 then
+
+            -- Try predicting demoman shield charge.
+            local stop = false
+            if (pLocal:InCond(17)) and PlayerClass == 4 or PlayerClass == 8 then -- If we are charging (17 is TF_COND_SHIELD_CHARGE)
+                stop = true
+                dynamicstop = 45
+                if (pCmd.forwardmove == 0) then dynamicstop = 30 end -- case if you dont hold w when charging
+                if closestDistance <= dynamicstop and PlayerClass == 4 then
+                    pCmd:SetButtons(pCmd:GetButtons() | IN_ATTACK)
+                end
+            end
+
+            if closingSpeed ~= 0 and closingspeed ~= nil then
                 estHitTime = distance / closingSpeed
-            elseif closingSpeedfeet ~= 0 and distance > distancefeet then
+            elseif closingSpeed ~= nil and closingSpeedfeet ~= 0 and distance > distancefeet then
                 estHitTime = distancefeet / closingSpeedfeet
             end
             
             -- If estimated hit time has not changed since last tick, set it to 0
-            if estHitTime == estHitTimeLast then
+            if estHitTime == estHitTimeLast or stop then
                 estHitTime = 0
             end
 
@@ -229,7 +240,6 @@ if Swingpred:GetValue() then
     end
         ::continue::
     end
-end
 
 -- ent_fire !picker Addoutput "health 99"
 local myfont = draw.CreateFont( "Verdana", 16, 800 ) -- Create a font for doDraw
