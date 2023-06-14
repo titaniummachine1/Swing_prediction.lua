528bf00d230e578a1dde7cf5e2972dfc17370097
titanium_machine <78664175+titaniummachine1@users.noreply.github.com>
2023-06-14 15:57:44 +0200

    v5.4.0

--- Swing_Prediction.lua
+++ Swing_Prediction.lua
@@ -354,7 +354,7 @@ local function OnCreateMove(pCmd)
     if pLocalClass ~= pLocalClasslast then
         if pLocal == nil then pLocalOrigin = pLocal:GetAbsOrigin() return pLocalOrigin end
         --get pLocal eye level and set vector at our eye level to ensure we cehck distance from eyes
-        local viewOffset = Vector3(0, 0, 70)
+        local viewOffset = pLocal:GetPropVector("localdata", "m_vecViewOffset[0]") --Vector3(0, 0, 70)
         local adjustedHeight = pLocal:GetAbsOrigin() + viewOffset
         viewheight = (adjustedHeight - pLocal:GetAbsOrigin()):Length()
             -- eye level 
